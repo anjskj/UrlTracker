950abe7af07643add67f1e14c34445ffc5d7f6c6
anjskj <45606242+anjskj@users.noreply.github.com>
2021-05-27 08:20:52 +0200

    added logic to app.js and empty ul in index

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-let inputBtn = document.getElementById('input-btn')
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById('input-el')
+const ulEl = document.getElementById('ul-el')
+
+const inputBtn = document.getElementById('input-btn')
 inputBtn.addEventListener('click', saveLead)
 
+
+// const viewBtn = document.getElementById('view-btn')
+// viewBtn.addEventListener('click', renderLead)
+
 function saveLead() {
-    console.log('button clicked')
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLead()
+
 }
 
+function renderLead() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `<li>${myLeads[i]}</li>`
+
+        // //alt
+        // const li = document.createElement('li')
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+    }
+    ulEl.innerHTML = listItems
+}
