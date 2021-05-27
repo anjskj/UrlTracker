698526190b8d8c850231d36f8421a7279d53b613
anjskj <45606242+anjskj@users.noreply.github.com>
2021-05-27 09:30:59 +0200

    more logic and styling

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
 
 const inputBtn = document.getElementById('input-btn')
 inputBtn.addEventListener('click', saveLead)
 
-
-// const viewBtn = document.getElementById('view-btn')
-// viewBtn.addEventListener('click', renderLead)
-
 function saveLead() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     renderLead()
-
 }
 
 function renderLead() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += `<li>${myLeads[i]}</li>`
-
+        listItems += `
+        <li>
+            <a target="_blank" href="http://${myLeads[i]}">
+                ${myLeads[i]}
+            </a>
+        </li>
+        `
         // //alt
         // const li = document.createElement('li')
         // li.textContent = myLeads[i]
