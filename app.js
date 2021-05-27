60bff25e9a802c5387117aae22f6e23532c7e6b5
anjskj <45606242+anjskj@users.noreply.github.com>
2021-05-27 11:09:00 +0200

    added url-tracker

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
 let myUrls = []
 
+let oldUrls = []
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
 
-const inputBtn = document.getElementById('input-btn')
-inputBtn.addEventListener('click', saveUrl)
-
-function saveUrl() {
-    myUrls.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myUrls", JSON.stringify(myUrls))
-
-    renderUrl()
-    var storedUrls = JSON.parse(localStorage.getItem("myUrls"));
-    console.log(localStorage.getItem("myUrls"))
+const storedUrls = JSON.parse(localStorage.getItem("myUrls"));
+if (storedUrls) {
+    myUrls = storedUrls
+    renderUrls(myUrls)
 }
-
-
-
-function renderUrl() {
+function renderUrls(urls) {
     let listItems = ""
-    for (let i = 0; i < myUrls.length; i++) {
+    for (let i = 0; i < urls.length; i++) {
         listItems += `
         <li>
-            <a target="_blank" href="${myUrls[i]}">
-                ${myUrls[i]}
+            <a target="_blank" href="${urls[i]}">
+                ${urls[i]}
             </a>
         </li>
         `
@@ -34,4 +25,34 @@ function renderUrl() {
         // ulEl.append(li)
     }
     ulEl.innerHTML = listItems
+
+}
+
+const inputBtn = document.getElementById('input-btn')
+inputBtn.addEventListener('click', saveInput)
+const deleteBtn = document.getElementById('delete-btn')
+deleteBtn.addEventListener('dblclick', deleteAll)
+const tabBtn = document.getElementById('tab-btn')
+tabBtn.addEventListener('click', saveTab)
+
+
+function saveInput() {
+    myUrls.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myUrls", JSON.stringify(myUrls))
+    renderUrls(myUrls)
+}
+
+function deleteAll() {
+    localStorage.clear()
+    myUrls = []
+    renderUrls(myUrls)
+}
+
+function saveTab() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myUrls.push(tabs[0].url)
+        localStorage.setItem("myUrls", JSON.stringify(myUrls))
+        renderUrls(myUrls)
+    })
 }
