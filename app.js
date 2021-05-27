4da50c043f7f1ad5eae577ad38775098c40a85da
anjskj <45606242+anjskj@users.noreply.github.com>
2021-05-27 09:56:39 +0200

    adding localstorage

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
-let myLeads = []
+let myUrls = []
+
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
 
 const inputBtn = document.getElementById('input-btn')
-inputBtn.addEventListener('click', saveLead)
+inputBtn.addEventListener('click', saveUrl)
 
-function saveLead() {
-    myLeads.push(inputEl.value)
+function saveUrl() {
+    myUrls.push(inputEl.value)
     inputEl.value = ""
-    renderLead()
+    localStorage.setItem("myUrls", JSON.stringify(myUrls))
+
+    renderUrl()
+    var storedUrls = JSON.parse(localStorage.getItem("myUrls"));
+    console.log(localStorage.getItem("myUrls"))
 }
 
-function renderLead() {
+
+
+function renderUrl() {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < myUrls.length; i++) {
         listItems += `
         <li>
-            <a target="_blank" href="http://${myLeads[i]}">
-                ${myLeads[i]}
+            <a target="_blank" href="${myUrls[i]}">
+                ${myUrls[i]}
             </a>
         </li>
         `
         // //alt
         // const li = document.createElement('li')
-        // li.textContent = myLeads[i]
+        // li.textContent = myUrls[i]
         // ulEl.append(li)
     }
     ulEl.innerHTML = listItems
